341f5d3bd689a38e6620c51b6d672e59dd336184
redstar125 <58530646+redstar125@users.noreply.github.com>
2023-01-05 16:09:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
 user:'batideco.fr@gmail.com',
-clientId:'967452195136-t3h3vg70o8695n1th17i0mb07oggo3s8.apps.googleusercontent.com',
-clientSecret:'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',
-refreshToken:'1//04O661uIyyou9CgYIARAAGAQSNwF-L9IrpTagX4eYaTvZZyijUM4HSqRU0w5Ae7GxcTtiQY2qLCNTl8LkYOB8UmImjMabE8tLjSQ'
+clientId:'967452195136-dittgpfidkamkjm4ijnicqsjceh6u153.apps.googleusercontent.com',
+clientSecret:'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',
+refreshToken:'1//045d-r83FlMyQCgYIARAAGAQSNwF-L9IrPh9SIKey0xmrLCXhFWvtGvElAdy3qfUDdDlIHwe8nogGYKaHqwANPqpX02ehW92LSAg'
 }
